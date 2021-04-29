fc65fd3a9fd93094599c94eaa040228b8fd89d10
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-04-29 17:19:30 +0200

    primo snack

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$primagiornata = [
+  [
+    'squadracasa' => 'napoli',
+    'squadraospite' => 'roma',
+    'punticasa' => '28',
+    'puntiospiti' => '64'
+  ]
+  [
+    'squadracasa' => 'fiorentina',
+    'squadraospite' => 'cerveteri',
+    'punticasa' => '75',
+    'puntiospiti' => '37'
+  ]
+];
+
+var_dump($primagiornata);
+
+ ?>
